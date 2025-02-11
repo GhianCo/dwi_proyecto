89f24765512ea15432a6ff3f9b05307d7e0b6813
GhianCo <ghiancorx@gmail.com>
2025-02-11 17:41:56 -0500

    cambios

--- src/main/java/shared/BaseServlet.java
+++ src/main/java/shared/BaseServlet.java
@@ -10,27 +10,22 @@ import java.io.IOException;
 
 public class BaseServlet extends HttpServlet {
     
-    // Método para manejar solicitudes OPTIONS (preflight)
     @Override
     protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException  , IOException {
-        // Permitir solicitudes de un origen específico o de todos los orígenes.
         String origin = request.getHeader("Origin");
         if (origin != null) {
             response.setHeader("Access-Control-Allow-Origin", origin); // Permitir el origen dinámicamente
         }
 
-        // Configuración de CORS
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Max-Age", "86400"); // Cachear la respuesta preflight por 24 horas
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         
-        // Permitir las cabeceras que se pueden usar en la solicitud.
         String requestHeaders = request.getHeader("Access-Control-Request-Headers");
         if (requestHeaders != null) {
             response.setHeader("Access-Control-Allow-Headers", requestHeaders);
         }
 
-        // Si la solicitud es OPTIONS, respondemos con 200 OK y no procesamos más
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
@@ -46,7 +41,6 @@ public class BaseServlet extends HttpServlet {
     protected void sendJsonResponse(HttpServletResponse response, Object data) throws IOException {
         allowCORS(response);
 
-        response.setContentType("application/json");
         JSONObject jsonResponse = new JSONObject(data);
         response.getWriter().write(jsonResponse.toString());
     }
